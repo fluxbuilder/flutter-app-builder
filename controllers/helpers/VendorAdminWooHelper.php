b442667ade6e1d252345638b4272e4a0200df105
vuthanhtoan30 <vuthanhtoan30@gmail.com>
2021-10-07 10:23:13 +0700

    remove wp-load

--- controllers/helpers/VendorAdminWooHelper.php
+++ controllers/helpers/VendorAdminWooHelper.php
@@ -1,5 +1,4 @@
 <?php
-require_once (ABSPATH . '/wp-load.php');
 
 class VendorAdminWooHelper
 {
@@ -18,7 +17,6 @@ class VendorAdminWooHelper
 
     protected function upload_image_from_mobile($image, $count, $user_id)
     {
-        require_once (ABSPATH . '/wp-load.php');
         require_once (ABSPATH . 'wp-admin' . '/includes/file.php');
         require_once (ABSPATH . 'wp-admin' . '/includes/image.php');
         $imgdata = $image;
